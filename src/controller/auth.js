5cc74695467a0ab57a40b2d1711999b90b2e0e4a
JungHoon Park <wfr112@naver.com>
2018-09-23 16:20:08 +0900

    Add comments in passport

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -11,7 +11,7 @@ module.exports = ({
     checkLoggedIn
   } = require('../middleware/authenticate')
 
-  //POST login 실패시 failureRedircet에 의해 작동되는 라우터
+  //@desc : login 실패시 failureRedircet에 의해 작동되는 라우터
   api.get('/session/fail', (req, res) => {
     if (req.flash)
       return res.status(400).json({
@@ -19,11 +19,9 @@ module.exports = ({
       })
   })
 
-  /*
-  passport에 의해서 작동됨 
-    usernameField: "email",
-    passwordField: "password",
-  */
+  //@desc : 로컬 로그인
+  //@router : POST http://localhost:3001/api/auth/session
+  //@body : email: String, password: String
   api.post('/session', passport.authenticate('local', {
     session: true,
     failureRedirect: '/api/auth/session/fail',
@@ -34,22 +32,29 @@ module.exports = ({
     })
   })
 
+  //@desc : 로컬 & 페이스북 로그아웃
+  //@router : GET http://localhost:3001/api/auth/session
   api.get('/session', (req, res) => {
     req.logout()
     res.status(200).json({})
   })
 
-  //@facebook router
-  api.get('/facebook', passport.authenticate('facebook', {scope: 'email'}))
+  //@desc : 페이스북 로그인
+  api.get('/facebook', passport.authenticate('facebook', {
+    scope: 'email'
+  }))
 
+  //@desc : 페이스북 로그인 실패, 성공시 거쳐가는 콜백 
   api.get('/facebook/callback', passport.authenticate('facebook', {
     successRedirect: '/api/users/me',
     failureRedirect: '/api/auth/facebook/fail'
   }))
 
-
+  //@desc : 페이스북 로그인 실패
   api.get('/facebook/fail', (req, res) => {
-    return res.status(500).json({message: errorMessage.FACEBOOK_LOGIN_FAIL})
+    return res.status(500).json({
+      message: errorMessage.FACEBOOK_LOGIN_FAIL
+    })
   })
 
   return api
