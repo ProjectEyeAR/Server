bad5be45bc78094f4f98b118b31f17fb1b5d6346
jourheee <jourheee@gmail.com>
2018-09-28 21:05:03 +0900

    Increase request body size limit

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(flash());
 app.disable('x-powered-by')
 app.use(helmet())
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false, limit: '100mb'}))
 app.use(bodyParser.json())
 
 app.use('/api', routes)
