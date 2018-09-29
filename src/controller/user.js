2a9df2b4f23f8bfd0d269b621d28ff5028f35c33
jourheee <jourheee@gmail.com>
2018-09-30 04:22:13 +0900

    no message

--- src/controller/user.js
+++ src/controller/user.js
@@ -103,7 +103,7 @@ module.exports = ({
 	//@desc : 특정 유저 정보 가져오기
 	//@router : GET http://localhost:3001/api/users/:id
 	//@params : id: String
-	api.get('/:id', [checkLoggedIn, checkIdParams], async (req, res) => {
+	api.get('/:id', checkIdParams, async (req, res) => {
 		let id = req.params.id
 
 		/*
@@ -121,25 +121,26 @@ module.exports = ({
 		let query = { '_id': id }
 		let user = await User.findOne(query)
 			
-			let followingQuery = { user: id }
-    		let followingCount = await Following.count(followingQuery)
+			let followingCountQuery = { user: id }
+    		let followingCount = await Following.count(followingCountQuery)
 
-    		let followerQuery = { followUser: id }
-    		let followerCount = await Following.count(followerQuery)
+    		let followerCountQuery = { followUser: id }
+    		let followerCount = await Following.count(followerCountQuery)
 
     		let commentCountQuery = { user: id }
     		let commentCount = await Comment.count(commentCountQuery)
 
-			/*
-			let userId = req.user._id
-			let query = { user: userId, followUser: id }
-			let count = await Following.count(query)
-			*/
-
 			user.set('followingCount', followingCount)
     		user.set('followerCount', followerCount)
     		user.set('commentCount', commentCount)
-			//user.set('following', count > 0 ? true : false)
+
+			if (req.isAuthenticated) {
+				let userId = req.user._id
+				let followingQuery = { user: userId, followUser: id }
+				let count = await Following.count(followingQuery)
+
+				user.set('following', count > 0)
+			}
 
 			return res.status(200).json({
 				data: user
