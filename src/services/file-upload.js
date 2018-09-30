674a4f49ff99260603d462ef6be96f89fa136a4a
JungHoon Park <wfr112@naver.com>
2018-09-30 20:22:11 +0900

    Change multerS3 to multerS3Transform

--- src/services/file-upload.js
+++ src/services/file-upload.js
@@ -3,7 +3,7 @@ module.exports = ({
 }) => {
     const aws = require('aws-sdk')
     const multer = require('multer')
-    const multerS3 = require('multer-s3')
+    const multerS3 = require('multer-s3-transform')
     const path = require('path')
     const sharp = require('sharp')
 
